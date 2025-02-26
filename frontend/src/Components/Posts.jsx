c3e134aef78cb775595c5438f812f86c4aa5c3a7
Misha <Misha@git.com>
2025-02-26 22:30:30 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/Components/Posts.jsx
+++ frontend/src/Components/Posts.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Posts = () => {
   return (
     <div>
+      <form action="" method="post" encType="multipart/form-data">
+        <input type="file" name="file[]" id="files" multiple />
+        <br />
+        <br />
+        <input
+          type="text"
+          name="caption"
+          id="caption"
+          placeholder="Write Something"
+        />
+        <br />
+        <br />
+        <input type="button" id="submit" value="Upload" />
+      </form>
+      <br />
+      <div id="preview"></div>
+      
 
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
