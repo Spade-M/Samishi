0d0cab4bbabcafc195323fd4676cf7155abe0e78
justsarx <sinhasarthak56@gmail.com>
2025-03-05 18:20:43 +0530

    Reapply "Adds liking and commenting functionality"
    This reverts commit 035300d86ae590f8b946b049b6c6cd627acd4d33.

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import SignupView, LoginView, UserDetailView, LogoutView, PostListCreateView, PostDetailView
+from .views import (
+    SignupView, 
+    LoginView, 
+    UserDetailView, 
+    LogoutView, 
+    PostListCreateView, 
+    PostDetailView,
+    PostLikeView,
+    PostCommentView
+)
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
@@ -7,7 +16,7 @@ urlpatterns = [
     path('user/', UserDetailView.as_view(), name='user-detail'),
     path('posts/', PostListCreateView.as_view(), name='post-list-create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-
+    path('posts/<int:pk>/like/', PostLikeView.as_view(), name='post-like'),
+    path('posts/<int:pk>/comments/', PostCommentView.as_view(), name='post-comment'),
     path('logout/', LogoutView.as_view(), name='logout'),
-
 ]
